87120087c2680ed4a9402b33f290f9ab357aedee
santiagopz2024 <valenciajennifer014@gmail.com>
2025-03-17 15:39:55 -0500

    Create Style.css

---
+++ css/Style.css
@@ -0,0 +1,28 @@
+body {
+    font-family: sans-serif;
+    text-align: center;
+}
+
+.grid {
+    display: grid;
+    grid-template-columns: repeat(3, 100px);
+    grid-gap: 10px;
+    margin: 20px auto;
+    width: 330px;
+}
+
+.cell {
+    width: 100px;
+    height: 100px;
+    background-color: #ccc;
+    border: 2px solid #999;
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 2em;
+}
+
+.cell.found {
+    background-color: lightgreen;
+}
